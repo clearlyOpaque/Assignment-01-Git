9e1bd3d19b9b524fbea498a82ed7350dd0cf17ae
gavinteet <gavinteet@gavins-mbp.unl.edu>
2018-09-07 14:24:41 -0500

    Added my name

--- Git Repository Assignment 01/src/HelloGavinAnthony.java
+++ Git Repository Assignment 01/src/HelloGavinAnthony.java
@@ -3,6 +3,7 @@ public class HelloGavinAnthony {
 
 	public static void main(String[] args) {
 		System.out.println("Anthony");
+		System.out.println("Gavin Yeet");
 		System.out.println("test 1");
 	}
 
